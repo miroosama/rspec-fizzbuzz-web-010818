7af7b142cb080d1ae0aa11c6942fe06cc0e62113
osama miro <o.miro650@gmail.com>
2017-12-15 03:12:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,12 @@
 def fizzbuzz(num)
   if num % 3 == 0
     fizz_3 = "Fizz"
-    return fizz_3
+   puts fizz_3
   elsif num % 5 == 0
     return ="Buzz"
   elsif num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
+  else 
+    
   end
 end
